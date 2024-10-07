3e36527ee57dc0b71ac720f4ae96cf3f7c790deb
JoaoNathanIf <119938590+JoaoNathanIf@users.noreply.github.com>
2024-10-07 15:42:25 +0000

    aula 07-10

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Footer from "./component /footer";
+import Navbar from "./component /navbar";
+import {Container,Row,Col} from 'react-bootstrap';
+export default function Index() {
+  return <>
+    <Navbar />
+    <Container>
+      <Row>
+        <Col className="bg-primary">1 of 2</Col>
+        <Col className="bg-info">2 of 2</Col>
+      </Row>
+      <Row>
+        <Col className = "bg-primary">1 of 3</Col> 
+        <Col className = "bg-primary">2 of 3</Col>
+        <Col className = "bg-primary">3 of 3</Col>
+      </Row>
+    </Container>
+    <Footer></Footer>
+  </>;
+}

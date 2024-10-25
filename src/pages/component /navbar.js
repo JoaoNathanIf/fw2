a94bde60e062c9c094291e79ff2c23ed30d1ec20
JoaoNathanIf <119938590+JoaoNathanIf@users.noreply.github.com>
2024-10-25 14:19:59 +0000

    aula dia 25-10

--- src/pages/component /navbar.js
+++ src/pages/component /navbar.js
@@ -2,18 +2,19 @@ import {Container,Nav,Navbar} from 'react-bootstrap';
 
 export default function Menu() {
   return (
+  <Container>
     <Navbar expand="sm" className="bg-info-subtle">
-      <Container>
         <Navbar.Brand href="/">6B</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/promessa">Promessa</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
     </Navbar>
+  </Container>
   );
 }
 
